07ad4636c9a70ff3e0b10bd924e6c62bbdb3f860
gguo <gguo@movoto.com>
2018-11-21 21:16:01 -0800

    added publish info

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="re_name",
-    version="0.0.2",
+    version="0.0.3",
     author="George Guo",
     author_email="geforcesong@gmail.com",
     description="A small example package",
