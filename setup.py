477b8bbeb3eba2835acb3d6c04163dc79108dad0
gguo <gguo@movoto.com>
2018-11-27 09:54:13 -0800

    updated ver

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="re_name",
-    version="0.0.8",
+    version="0.0.9",
     author="George Guo",
     author_email="geforcesong@gmail.com",
     description="A small example package",
