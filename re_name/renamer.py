ecb04a0c32c11aef3012c4d855fe62a38535f795
gguo <gguo@movoto.com>
2018-11-27 09:51:44 -0800

    added rename functionality

--- re_name/renamer.py
+++ re_name/renamer.py
@@ -1,16 +1,18 @@
 import os
 import argparse
+import re
 
 
 class Renamer:
 
     def __init__(self):
-        # self.currentPath = os.getcwd()
-        self.currentPath = '/Users/george/lll/'
+        self.currentPath = os.getcwd()
+        # self.currentPath = '/Users/george/lll/'
         self.oldPatten = ''
         self.newPatten = ''
         self.isPreview = False
-        self.parseInputs()
+        self.parser = argparse.ArgumentParser()
+        self.parseInputs(self.parser)
         print('Working Path:' + self.currentPath)
 
     def update(self):
@@ -18,11 +20,13 @@ class Renamer:
         if(len(files) > 0):
             for file in files:
                 if(os.path.isfile(os.path.join(self.currentPath, file))):
-                    print(file)
-        print('Renamer update is called!')
+                    newfileName = re.sub(self.oldPatten, self.newPatten, file)
+                    if not self.isPreview:
+                        os.rename(os.path.join(self.currentPath, file),
+                                  os.path.join(self.currentPath, newfileName))
+                    print(file, ' ----> ', newfileName)
 
-    def parseInputs(self):
-        parser = argparse.ArgumentParser()
+    def parseInputs(self, parser):
         parser.add_argument("old", help="specify old patten to remove")
         parser.add_argument("new", help="specify new patten you wanted")
         parser.add_argument("-p", "--preview", action="store_true",
@@ -33,7 +37,6 @@ class Renamer:
         if(args.new):
             self.newPatten = args.new.strip()
         self.isPreview = args.preview
-        print(self.oldPatten, self.newPatten)
         # if(self.oldPatten == "" or self.newPatten == ""):
         #     print("You haven't spcify valid parameters, use --help for usage")
         #     os._exit(1)
