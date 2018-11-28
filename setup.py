e93f84f1da663f8de906efb93412d51f964b218b
gguo <gguo@movoto.com>
2018-11-28 09:58:17 -0800

    added command for windows

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="re_name",
-    version="0.0.13",
+    version="0.0.14",
     author="George Guo",
     author_email="geforcesong@gmail.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/geforcesong/re_name",
-    scripts=['bin/re_name'],
+    scripts=['bin/re_name', 'bin/re_name.cmd'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
